bedc023aa819458d101eb456b993a6a39924a9de
matt <matt.almeidac@gmail.com>
2026-02-19 23:06:19 -0300

    fundo pulse, botao resume e game save setado

---
+++ save.gd
@@ -0,0 +1,1 @@
+extends "res://scripts/save_game.gd"
--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -1,16 +1,54 @@
 extends Control
 
+@onready var save: Node = %Save
+@onready var resume: Button = %resume
 
-# Called when the node enters the scene tree for the first time.
+@onready var back_light: TextureRect = $backLight
+@export var pulse :bool = true
+@export var curve :Curve = Curve.new()
+@export_range(0.0,1.0) var alpha_min :float = 0.2
+@export_range(0.0,1.0) var alpha_max :float = 0.3
+
+var change :float
+var dir = 1
+var time = 0
+
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	if save.load_Save() == null:
+		resume.visible = false
+	else:
+		resume.visible = true
+		
+	change = alpha_min
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	
+	if pulse == true:
+		transparency_flow()
 
 
+func transparency_flow() -> void:
+	time += get_process_delta_time() * dir * 0.5
+	
+	
+	if time >= 1.0: dir = -1
+	elif time <= 0: dir = 1
+	
+	change = alpha_min + time * alpha_max
+	
+	back_light.self_modulate = Color(1.0,1.0,1.0,change)
+	
+	#print(curve.sample(time))
+	print(curve)
+	#print(change, " rate: ",rate)
+
+
+
+func _on_resume_pressed() -> void:
+	save.load_Save()
 
 
 func _on_quit_pressed() -> void:
